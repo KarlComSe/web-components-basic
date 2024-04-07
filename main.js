90af1787e46f9319b9b6d782d708384feb2a8a39
KarlComSe <karl@wackerberg.se>
2024-04-07 22:01:30 +0200

    Register custom element wc-main and order-list in the current context

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import WcHeader from "./components/wc-header.js";
 import WcMain from "./components/wc-main.js";
+import OrderList from "./components/order-list.js";
 
 customElements.define("wc-header", WcHeader, { extends: "header" });
 customElements.define("wc-main", WcMain, { extends: "main" });
+customElements.define("order-list", OrderList);
