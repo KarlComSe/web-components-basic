ed503d0d04c9545f9eb5a4f2edd878ba9828a331
KarlComSe <karl@wackerberg.se>
2024-04-08 00:41:20 +0200

    Improve components highlight through text and styling to make it clearer which components are which

--- components/wc-header.js
+++ components/wc-header.js
@@ -6,9 +6,17 @@ class WcHeader extends HTMLElement {
 
     connectedCallback() {
         this.innerHTML = `
+            <style>
+            .header {
+                border: 5px solid red;
+                padding: 10px;
+            }
+            </style>
+            <div class="header">
                 <h1>Orders</h1>
                 <p>Example page for web components, this is the header-element which is specified with is = "wc-header".</p>
                 <p>This doesn't utilize the shadow DOM.</p>
+            </div>
         `;
     }
 }
--- components/wc-main.js
+++ components/wc-main.js
@@ -8,6 +8,13 @@ class WcMain extends HTMLElement {
       mode: 'open'
     });
     shadowRoot.innerHTML = `
+            <style>
+                .main {
+                    border: 5px solid coral;
+                    padding: 10px;
+                }
+            </style>
+            <div class="main">
             <h2>
                 <slot name="title">Default text which should be replaced</slot>
             </h2>
@@ -15,6 +22,7 @@ class WcMain extends HTMLElement {
             <p>This utilize the shadow DOM, as it is required for slots to work.</p>
             <p>This utilizes slots, to replace the title.</p>
             <slot></slot>
+            </div>
         `;
   }
 }
