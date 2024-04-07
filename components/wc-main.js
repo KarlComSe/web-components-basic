e8631797518038e0669a2a63bcec91bd01827af4
KarlComSe <karl@wackerberg.se>
2024-04-07 13:27:45 +0200

    Add main web component, which utilize shadow dom and slots

---
+++ components/wc-main.js
@@ -0,0 +1,21 @@
+class WcMain extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    const shadowRoot = this.attachShadow({
+      mode: 'open'
+    });
+    shadowRoot.innerHTML = `
+            <h2>
+                <slot name="title">Default text which should be replaced</slot>
+            </h2>
+            <p>This is the main-element which is specified with is = "wc-main".</p>
+            <p>This utilize the shadow DOM, as it is required for slots to work.</p>
+            <p>This utilizes slots, to replace the title.</p>
+        `;
+  }
+}
+
+export default WcMain;
--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 import WcHeader from "./components/wc-header.js";
+import WcMain from "./components/wc-main.js";
 
 customElements.define("wc-header", WcHeader, { extends: "header" });
+customElements.define("wc-main", WcMain, { extends: "main" });
